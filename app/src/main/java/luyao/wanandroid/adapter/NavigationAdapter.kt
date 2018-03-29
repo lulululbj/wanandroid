5e207e152d5975bdcc2bf8f66acf3ded973c3d0b
luyao <sunluyao1993x@gmail.com>
2018-03-29 23:25:01 +0800

    add tagFlowLayout

---
+++ app/src/main/java/luyao/wanandroid/adapter/NavigationAdapter.kt
@@ -0,0 +1,50 @@
+package luyao.wanandroid.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.zhy.view.flowlayout.FlowLayout
+import com.zhy.view.flowlayout.TagAdapter
+import com.zhy.view.flowlayout.TagFlowLayout
+import luyao.wanandroid.R
+import luyao.wanandroid.bean.Article
+import luyao.wanandroid.bean.Navigation
+import luyao.wanandroid.ui.BrowserActivity
+
+
+/**
+ * Created by Lu
+ * on 2018/3/29 22:36
+ */
+class NavigationAdapter(layoutResId: Int = R.layout.item_navigation) : BaseQuickAdapter<Navigation, BaseViewHolder>(layoutResId) {
+
+    override fun convert(helper: BaseViewHolder, item: Navigation) {
+
+        helper.setText(R.id.navigationName, item.name)
+        helper.getView<TagFlowLayout>(R.id.navigationTagLayout).run {
+            adapter = object : TagAdapter<Article>(item.articles) {
+                override fun getCount(): Int {
+                    return item.articles.size
+                }
+
+                override fun getView(parent: FlowLayout, position: Int, t: Article): View {
+                    val tv = LayoutInflater.from(parent.context).inflate(R.layout.tag,
+                            parent, false) as TextView
+                    tv.text = t.title
+                    return tv
+                }
+            }
+
+            setOnTagClickListener({ _, position, parent ->
+                val intent = Intent(parent.context, BrowserActivity::class.java)
+                intent.putExtra(BrowserActivity.URL, item.articles[position].link)
+                parent.context.startActivity(intent)
+                true
+            })
+        }
+    }
+
+}
--- app/src/main/java/luyao/wanandroid/ui/navigation/NavigationFragment.kt
+++ app/src/main/java/luyao/wanandroid/ui/navigation/NavigationFragment.kt
@@ -1,10 +1,14 @@
 package luyao.wanandroid.ui.navigation
 
+import android.support.v7.widget.LinearLayoutManager
+import dp2px
 import kotlinx.android.synthetic.main.fragment_navigation.*
 import luyao.gayhub.base.BaseMvpFragment
 import luyao.wanandroid.R
+import luyao.wanandroid.adapter.NavigationAdapter
 import luyao.wanandroid.adapter.VerticalTabAdapter
 import luyao.wanandroid.bean.Navigation
+import luyao.wanandroid.view.SpaceItemDecoration
 
 /**
  * Created by Lu
@@ -14,11 +18,17 @@ class NavigationFragment : BaseMvpFragment<NavigationContract.View, NavigationPr
 
     private val navigationList = mutableListOf<Navigation>()
     private val tabAdapter by lazy { VerticalTabAdapter(navigationList.map { it.name }) }
+    private val navigationAdapter by lazy { NavigationAdapter() }
     override var mPresenter = NavigationPresenter()
 
     override fun getLayoutResId() = R.layout.fragment_navigation
 
     override fun initView() {
+        navigationRecycleView.run {
+            layoutManager = LinearLayoutManager(activity)
+            addItemDecoration(SpaceItemDecoration(this.dp2px(10f)))
+            adapter = navigationAdapter
+        }
     }
 
     override fun initData() {
@@ -29,6 +39,8 @@ class NavigationFragment : BaseMvpFragment<NavigationContract.View, NavigationPr
         this.navigationList.clear()
         this.navigationList.addAll(navigationList)
         tabLayout.setTabAdapter(tabAdapter)
+
+        navigationAdapter.setNewData(navigationList)
     }
 
 }
