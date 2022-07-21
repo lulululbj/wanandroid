21c6d58a64367514a2f5aeb5b0375b0f86237f7e
luyao <sunluyao1993x@gmail.com>
2022-07-21 10:41:17 +0800

    proguard

--- app/src/main/java/luyao/wanandroid/ui/BrowserActivity.kt
+++ app/src/main/java/luyao/wanandroid/ui/BrowserActivity.kt
@@ -1,11 +1,5 @@
 package luyao.wanandroid.ui
 
-import android.graphics.Bitmap
-import android.util.Log
-import android.view.View
-import com.tencent.smtt.sdk.WebChromeClient
-import com.tencent.smtt.sdk.WebView
-import com.tencent.smtt.sdk.WebViewClient
 import kotlinx.android.synthetic.main.activity_browser.*
 import kotlinx.android.synthetic.main.activity_system_detail.*
 import luyao.mvvm.core.base.BaseActivity
@@ -33,45 +27,17 @@ class BrowserActivity : BaseActivity() {
         mToolbar.setNavigationOnClickListener { onBackPressed() }
 
         intent?.extras?.getString(URL).let {
-            webView.loadUrl(it)
         }
     }
 
     private fun initWebView() {
         progressBar.progressDrawable = this.resources
-                .getDrawable(R.drawable.color_progressbar)
-        webView.run {
-            webViewClient = object : WebViewClient() {
+            .getDrawable(R.drawable.color_progressbar)
 
-                override fun onPageStarted(p0: WebView?, p1: String?, p2: Bitmap?) {
-                    super.onPageStarted(p0, p1, p2)
-                    progressBar.visibility = View.VISIBLE
-                }
-
-                override fun onPageFinished(p0: WebView?, p1: String?) {
-                    super.onPageFinished(p0, p1)
-                    progressBar.visibility = View.GONE
-                }
-            }
-            webChromeClient = object : WebChromeClient() {
-                override fun onProgressChanged(p0: WebView?, p1: Int) {
-                    super.onProgressChanged(p0, p1)
-                    progressBar.progress = p1
-                    Log.e("browser", p1.toString())
-                }
-
-                override fun onReceivedTitle(p0: WebView?, p1: String?) {
-                    super.onReceivedTitle(p0, p1)
-                    p1?.let { mToolbar.title = p1 }
-                }
-
-            }
-        }
     }
 
     override fun onBackPressed() {
-        if (webView.canGoBack()) webView.goBack()
-        else super.onBackPressed()
+        super.onBackPressed()
     }
 
 
